7ca4c96be264a31dba80ab3ef1076cb426a3f5cd
Dinesh Rout <rddinesh779@gmail.com>
2022-07-23 15:04:48 +0530

    routes: user

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const { getTodoById } = require('../controllers/todo');
+const { getUser, getAllUsers, getUserById } = require('../controllers/user');
+
+const router = require('express').Router();
+
+// route params
+router.param('todoId', getTodoById);
+router.param('userId', getUserById);
+
+// get user
+router.get('/:userId', getUser);
+
+// get all users
+router.get('/:userId', getAllUsers);
+
+module.exports = router;

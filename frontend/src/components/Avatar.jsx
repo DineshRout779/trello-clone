6ab316041fe8ce518f76951cd5159c40a7f3d4a4
Dinesh Rout <rddinesh779@gmail.com>
2022-07-24 01:38:25 +0530

    code refctor 🔨

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,7 +1,12 @@
+import { useSelector } from 'react-redux';
+
 const Avatar = () => {
+  const { userInfo } = useSelector((state) => state.user);
+  let name = userInfo.fullName.split(' ')[0] || 'user';
+
   return (
     <div className='flex items-center avatar'>
-      <p>Hi Dinesh</p>
+      <p>Hi {name ? name : 'User'}</p>
       <img src='./images/avatar-6.png' alt='profile' className='avatar-img' />
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,7 @@ import AppRoutes from './routes/AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <AppRoutes />
+  </Provider>
 );

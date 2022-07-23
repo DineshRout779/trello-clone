acbfd0faea7ea6776d2187cfa268e112073fea17
Dinesh Rout <rddinesh779@gmail.com>
2022-07-24 01:39:56 +0530

    code refactor 🔨

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,26 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const { userInfo } = useSelector((state) => state.user);
   return (
     <div className='full-height home flex-center flex-col'>
       <img src='./images/todo.png' alt='todo' className='home-img' />
       <h1>Welcome to Todo App!</h1>
-      <div className='btn-group flex flex-gap-1'>
-        <Link to='login' className='btn'>
-          Login
+      {userInfo ? (
+        <Link to='/dashboard/projects' className='btn mt-1'>
+          Go to Dashboard
         </Link>
-        <Link to='signup' className='btn btn-rev'>
-          Sign up
-        </Link>
-      </div>
+      ) : (
+        <div className='btn-group flex flex-gap-1'>
+          <Link to='login' className='btn'>
+            Login
+          </Link>
+          <Link to='signup' className='btn btn-rev'>
+            Sign up
+          </Link>
+        </div>
+      )}
     </div>
   );
 };

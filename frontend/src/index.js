234eec1523817c46aebefb9c61b034a81d4bd015
Dinesh Rout <rddinesh779@gmail.com>
2022-07-23 15:08:32 +0530

    refactor: added redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import './index.css';
 import AppRoutes from './routes/AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRoutes />
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   </React.StrictMode>
 );

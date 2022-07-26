8523b1d77e26f90384a855c4070220c1d3c937b5
Dinesh Rout <rddinesh779@gmail.com>
2022-07-26 12:08:58 +0530

    added users list

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,5 +1,56 @@
 import { IoMdClose } from 'react-icons/io';
 
+const members = [
+  {
+    id: 1,
+    pic: 'avatar-6.png',
+    name: 'Saundarya ',
+    email: 'saundarya@idc.com',
+  },
+  {
+    id: 2,
+    pic: 'avatar-1.png',
+    name: 'Vaibhav',
+    email: 'vaibhav@idc.com',
+  },
+  {
+    id: 3,
+    pic: 'avatar-2.png',
+    name: 'sudhanshu',
+    email: 'sudhanshu@idc.com',
+  },
+  {
+    id: 4,
+    pic: 'avatar-3.png',
+    name: 'Shruti',
+    email: 'shruti@idc.com',
+  },
+  {
+    id: 5,
+    pic: 'avatar-4.png',
+    name: 'Himanshu',
+    email: 'himanshu@idc.com',
+  },
+  {
+    id: 6,
+    pic: 'avatar-2.png',
+    name: 'sudhanshu',
+    email: 'sudhanshu@idc.com',
+  },
+  {
+    id: 7,
+    pic: 'avatar-3.png',
+    name: 'Shruti',
+    email: 'shruti@idc.com',
+  },
+  {
+    id: 8,
+    pic: 'avatar-4.png',
+    name: 'Himanshu',
+    email: 'himanshu@idc.com',
+  },
+];
+
 const Modal = ({ setIsModalOpen }) => {
   const handleClick = (e) => {
     if (
@@ -10,15 +61,30 @@ const Modal = ({ setIsModalOpen }) => {
       setIsModalOpen(false);
     }
   };
+
   return (
     <div className='modal-container' id='modal-container' onClick={handleClick}>
       <div className='modal'>
         <div className='flex space-between items-center'>
-          <p className='modal-title'>Project members</p>
+          <h2 className='modal-title'>Project members</h2>
           <button className='btn-close' id='modal-close' onClick={handleClick}>
             <IoMdClose id='modal-close-btn' />
           </button>
         </div>
+
+        <ul>
+          {members.map((member) => {
+            return (
+              <li className='flex' key={member.id}>
+                <img src={`/images/${member.pic}`} alt='' />
+                <div>
+                  <h3>{member.name}</h3>
+                  <p>{member.email}</p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
